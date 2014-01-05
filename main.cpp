7a6c756693a4fd9fab997b907ecb75cfe47c3892
Sai Koppula <sai.koppula@gmail.com>
2014-01-04 22:43:05 -0600

    Makefile Set Up
    Basic Skeleton Files and Compile Script Created and Working.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * Executes program
+ * @date January 2014
+ * @author Sai Koppula
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    cout << "Program Beginning" << endl;
+    
+}
